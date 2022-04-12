02e07ebb9e3e81f4d3bf7748954829e9685d2ff2
grndng <gueren.dinga@hcu-hamburg.de>
2022-04-12 16:04:01 +0200

    Added todos and some weird stuff for filenames and folders, all hard coded

--- main.py
+++ main.py
@@ -1,20 +1,24 @@
 from PIL import Image, ImageFilter
 
 #TODO: Proper docstrings
+#TODO: package and argparse it!
+#TODO: Offer sample colors (pastel palette?)
+#TODO: GUI it?
 
-input = "./sample_images/b.jpg"
+input = "./sample_images/a.png"
+*_, filename = input.split("/")
+out = f"./sample_images/bg_{filename}"
 color = "#c3c3c3"
 
-def prettify(input, bgcolor="#c3c3c3", bordersize=100):
-    foreground = Image.open(input)
+def prettify(input, output, bgcolor="#c3c3c3", bordersize=100, bluramount=10):
+    im = Image.open(input)
 
-    w, h = foreground.size
+    w, h = im.size
     background = Image.new(mode = "RGB", size=(w+bordersize*2, h+bordersize*2), color=bgcolor)
-            
+
     background.paste(0, box=(bordersize, bordersize, w+bordersize, h+bordersize))
-    
-    blur = background.filter(ImageFilter.GaussianBlur(radius=5))
-    blur.paste(foreground, box=(bordersize, bordersize))
-    blur.save("blur_foreground.png")
+    blur = background.filter(ImageFilter.GaussianBlur(radius=bluramount))
+    blur.paste(im, box=(bordersize, bordersize))
+    blur.save(output)
         
-prettify(input)
+prettify(input, out, "#c3c3c3", 100, 2)
