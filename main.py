6fc4014d3eb2fd994fe4dd5b06f091291cca3cd1
grndng <gueren.dinga@hcu-hamburg.de>
2022-04-12 16:34:42 +0200

    Converting RGB to hex with average image color to pass it as background color later on

--- main.py
+++ main.py
@@ -14,20 +14,23 @@ out = f"./sample_images/bg_{filename}"
 color = "#c3c3c3"
 
 def compute_average_rgb_val(input) -> Tuple:
-    avg_color = [mean(input.getdata(band)) for band in range(len(input.mode))]
-    return avg_color
+    avg_color_rgb = tuple([int(mean(input.getdata(band))) for band in range(len(input.mode))])
+    R,G,B = avg_color_rgb
+    avg_color_hex = f"#{R:02x}{G:02x}{B:02x}"
+    return avg_color_hex
 
+print(compute_average_rgb_val(Image.open(input)))
 
 def prettify(input, output, bgcolor="#c3c3c3", bordersize=100, bluramount=10) -> None:
     im = Image.open(input)
 
     w, h = im.size
-    background = Image.new(mode = "RGB", size=(w+bordersize*2, h+bordersize*2), color=bgcolor)
 
+    background = Image.new(mode = "RGB", size=(w+bordersize*2, h+bordersize*2), color=bgcolor)
     background.paste(0, box=(bordersize, bordersize, w+bordersize, h+bordersize))
     blur = background.filter(ImageFilter.GaussianBlur(radius=bluramount))
     blur.paste(im, box=(bordersize, bordersize))
     blur.save(output)
     return None
         
-prettify(input, out, "#97C1A9", 100, 2)
+prettify(input, out, f"{compute_average_rgb_val(Image.open(input))}", 100, 2)
